0ae9f15cc4307bf00e21813f866e8a625c8621fe
Giovanni Palusa <giovanni@palusa.se>
2017-09-08 15:25:47 +0200

    Initial Commit

---
+++ Smask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smask
+//
+//  Created by Giovanni Palusa on 2017-09-08.
+//  Copyright © 2017 Giovanni Palusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
